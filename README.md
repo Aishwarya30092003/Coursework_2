df9f64889eac1c7d2caee3a3058efa9bfef51235
Aishwarya Sharma <aishwarya.abss@gmail.com>
2024-02-11 23:54:14 +0400

    Final

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Coursework_2
+Link to coursework 1 repository link
+https://github.com/Aishwarya30092003/Coursework_1.git
+
+Link to coursework 2 repository link
+https://github.com/Aishwarya30092003/Coursework_2.git
+
+Coursework 1 - Github pages link 
+https://aishwarya30092003.github.io/Coursework_1/ 
+
+Coursework 2- Github Pages Link 
+https://aishwarya30092003.github.io/Coursework_2/index.html 
+
+AWS main link
+http://afterschool-env.eba-3ppfxag2.eu-north-1.elasticbeanstalk.com/ 
+
+AWS Fetch Links
+
+Displaying Products
+http://afterschool-env.eba-3ppfxag2.eu-north-1.elasticbeanstalk.com/collection/Products 
+
+Search Products
+http://afterschool-env.eba-3ppfxag2.eu-north-1.elasticbeanstalk.com/collection/Products/search/
+
+Display Orders
+http://afterschool-env.eba-3ppfxag2.eu-north-1.elasticbeanstalk.com/collection/Orders
+
+Update available inventory in products
+http://afterschool-env.eba-3ppfxag2.eu-north-1.elasticbeanstalk.com/collection/Products/updateInventory
+
--- server.js
+++ server.js
@@ -1,127 +1,130 @@
-const express = require('express')
+// Import required modules
+const express = require('express');
 const path = require('path');
 
-const app = express()
+// Create an Express application
+const app = express();
 
-app.use(express.json())
-app.set('port', 3000)
+app.use(express.json());
+
+// Set the port number
+app.set('port', 3000);
+
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
-})
-
-// Logger middleware function
-app.use((req, res, next) => {
-    // Log the request method and URL
-    console.log(`[${new Date().toUTCString()}] ${req.method} ${req.originalUrl}`);
-    next()
-    // Log request body if it exists
 });
-const MongoClient = require('mongodb').MongoClient;
 
+// Middleware for logging requests
+app.use((req, res, next) => {
+    console.log(`[${new Date().toUTCString()}] ${req.method} ${req.originalUrl}`);
+    next();
+});
+
+// Connect to MongoDB database
+const MongoClient = require('mongodb').MongoClient;
 let db;
 MongoClient.connect('mongodb+srv://aishwaryaabss:Aishmal%401@gettingstarted.tpzj3aa.mongodb.net', (err, client) => {
-    db = client.db('AfterSchool')
+    db = client.db('AfterSchool');
+});
 
-})
-
+// Middleware to handle collection name parameter
 app.param('collectionName', (req, res, next, collectionName) => {
-    req.collection = db.collection(collectionName)
-    return next()
-})
+    req.collection = db.collection(collectionName);
+    return next();
+});
+
+// Routes
 app.get('/', (req, res, next) => {
-    res.send('select a collection, e.g., /collection/messages')
-})
+    res.send('select a collection, e.g., /collection/messages');
+});
 
-// app.listen(3000, () => {
-//     console.log('Express.js server running at localhost:3000')
-// })
-
+// Get all documents in a collection
 app.get('/collection/:collectionName', (req, res, next) => {
     req.collection.find({}).toArray((e, results) => {
-        if (e) return next(e)
-        res.send(results)
-    })
-})
+        if (e) return next(e);
+        res.send(results);
+    });
+});
 
+// Insert a new document into a collection
 app.post('/collection/:collectionName', (req, res, next) => {
     req.collection.insert(req.body, (e, results) => {
-        if (e) return next(e)
-        res.send(results.ops)
-    })
-})
+        if (e) return next(e);
+        res.send(results.ops);
+    });
+});
 
-const ObjectID = require('mongodb').ObjectID;
+// Get a document by ID from a collection
 app.get('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
-        if (e) return next(e)
-        res.send(result)
-    })
-})
+        if (e) return next(e);
+        res.send(result);
+    });
+});
 
+// Update inventory of a product
 app.put('/collection/:collectionName/updateInventory/:id', (req, res, next) => {
     req.collection.update(
         { _id: new ObjectID(req.params.id) },
         { $set: req.body },
         { safe: true, multi: false },
         (e, result) => {
-            if (e) return next(e)
-            res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
-        })
-})
+            if (e) return next(e);
+            res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' });
+        });
+});
 
+// Delete a document by ID from a collection
 app.delete('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.deleteOne(
         { _id: new ObjectID(req.params.id) },
         (e, result) => {
-            if (e) return next(e)
-            res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
-        })
-})
+            if (e) return next(e);
+            res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' });
+        });
+});
 
+// Search products in a collection based on query
 app.get('/collection/:collectionName/search/:searchQ', (req, res, next) => {
-    const query = req.params.searchQ; // Retrieve the search query parameter
-
-    let filter = {}; // Define an empty filter object
+    const query = req.params.searchQ; 
+    let filter = {}; 
 
     if (query) {
-        // If a search query is provided, construct a filter to search by title or description
         filter = {
             $or: [
-                { title: { $regex: query, $options: 'i' } }, // Case-insensitive search by title // Case-insensitive search by description
+                { title: { $regex: query, $options: 'i' } }, 
             ]
         };
     }
 
-    // Use the filter to find matching classes
     db.collection('Products').find(filter).toArray((err, results) => {
         if (err) {
             console.error('Error Searching:', err);
             return res.status(500).json({ error: 'Failed to search product' });
         }
-        res.send(results); // Send the search results back to the client
+        res.send(results);
     });
 });
 
+// Serve static images
 const STATIC_IMAGE_DIR = path.join(__dirname, 'static', 'images');
-
-// Set up middleware to serve static files (images in this case)
 app.use('/images', express.static(STATIC_IMAGE_DIR));
-
-// Define a route to handle image requests
 app.get('/images/:imageFilename', (req, res) => {
     const imageFilename = req.params.imageFilename;
     res.sendFile(path.join(STATIC_IMAGE_DIR, imageFilename));
 });
 
-// Error handler middleware
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Image Not Found');
 });
 
-const port = process.env.PORT || 3000
-app.listen(port)
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port);
